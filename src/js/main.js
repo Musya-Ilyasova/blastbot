c436925fdc7eb4116303a708a9c0b96be25727dd
Musya <musya.ilyasova@getorca.app>
2024-02-05 18:53:46 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 import addMenu from './modules/menu';
 import headerOnScroll from './modules/headerOnScroll';
 import scrollToTheSection from './modules/scrollToTheSection';
+import wow from './modules/animated';
+import customVh from './modules/customVh'
 
 document.addEventListener("DOMContentLoaded",  () => {
   addMenu();
   headerOnScroll();
   scrollToTheSection();
+  wow.init();
+  customVh();
 });
 
